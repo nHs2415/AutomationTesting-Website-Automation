e9dc64f4e8f62627ae5869796240b64c0cd0fdac
nHs2415 <nuwanihansika92@gmail.com>
2025-12-14 19:18:45 +0530

    Adding Cucumber Hooks and WebDriver factory for browser setup

---
+++ src/test/java/Congfig/DriverFactory.java
@@ -0,0 +1,32 @@
+package Congfig;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class DriverFactory {
+
+    public static WebDriver driver;
+
+    public static WebDriver initBrowser(String browser ){
+        if (browser.equalsIgnoreCase("chrome")){
+            WebDriverManager.chromedriver().setup();
+            driver = new ChromeDriver();
+        }
+        else {
+            throw new RuntimeException("We don't support this browser at the moment");
+        }
+        return driver;
+    }
+
+    public static WebDriver getDriver(){
+        return driver;
+    }
+
+    public static void quitDriver(){
+        if (driver != null){
+            driver.quit();
+            driver = null;
+        }
+    }
+}
---
+++ src/test/java/Hooks/Hooks.java
@@ -0,0 +1,22 @@
+package Hooks;
+
+import Congfig.DriverFactory;
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import org.openqa.selenium.WebDriver;
+
+public class Hooks {
+    private WebDriver webDriver;
+
+    @Before
+    public void setUp(){
+        webDriver = DriverFactory.initBrowser("chrome");
+        webDriver.manage().window().maximize();
+        webDriver.get("https://automationexercise.com/");
+    }
+
+    @After
+    public void tearDown(){
+        DriverFactory.quitDriver();
+    }
+}
